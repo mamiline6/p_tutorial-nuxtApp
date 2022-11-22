8476deea47a9173e43e910e9fa45c26ee0f33b79
mamiOkano <mirrorshock5112@yahoo.co.jp>
2022-11-23 01:13:26 +0900

    first commit

---
+++ components/LayoutWrapper.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="wrapper">
+    <slot />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'LayoutWrapper', // 使うときは <layout-wrapper>…</layout-wrapper>
+  }
+</script>
+
+<style>
+.wrapper {
+  padding-top: 80px;
+  @media(min-width: 768px) {
+    padding-top: 0;
+  }
+}
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,67 @@
+// evnファイルから必要な環境変数を定義
+const { API_KEY, API_URL } = process.env
+
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    titleTemplate(title) {
+      return (title ? `${title} | ` : '') + process.env.npm_package_name
+    },
+
+    htmlAttrs: {
+      lang: 'jp',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/',
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+
+  generate: {
+    fallback: true,
+  },
+  publicRuntimeConfig: {
+    apiUrl: API_URL,
+  },
+  privateRuntimeConfig: {
+    apiKey: API_KEY,
+  },
+}
